e1649edb3465744d90f7d1d15bf0e258c93d5f51
Babarino <seanbrad@asu.edu>
2026-06-06 22:35:11 -0700

    Update top section

--- src/routes/home.js
+++ src/routes/home.js
@@ -4,8 +4,8 @@ const router = express.Router();
 router.get('/', (req, res) => {
   res.render('home', {
     title: 'Healer',
-    tagline: 'Rebuild Yourself. One Day at a Time.',
-    description: 'A gamified self-growth app that helps you rebuild habits after breakups or life changes. Complete quests, gain EXP, and level up your life.',
+    tagline: 'ITS OK TO FEEL BROKEN WHAT MATTERS IS HOW YOU REBUILD.',
+    description: '“Turn your healing into progress. Complete simple daily tasks to gain experience, level up your character, and slowly rebuild yourself after heartbreak. Share your thoughts and stories in a supportive community, where others understand what you’re going through. If you need deeper support, access guided therapy tools designed to help you reflect, cope, and grow stronger—one step at a time.”.',
     author: 'Xun Yang Leong'
   });
 });
